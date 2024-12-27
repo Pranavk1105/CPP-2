c83818a348cca41a104514a50f034076c6605642
Pranavk1105 <pranavkale2806@gmail.com>
2024-12-27 21:44:54 +0530

    Add files via upload

---
+++ displaynum.cpp
@@ -0,0 +1,29 @@
+// #include <iostream>
+// using namespace std;
+// void displaynum(int n1, float n2){
+//             cout << "the int number is :" << n1 << endl;
+//             cout << "the doubal number is :" << n2;
+// }
+// int main() 
+// {
+//             int num1=10;
+//             float num2=10.512;
+//             displaynum(num1,num2);
+//   return 0;
+// }
+
+#include <iostream>
+using namespace std;
+int add(int n1 , int n2){
+            return(n1+n2);
+            int n1;
+            int n2;
+}
+int main() 
+{
+            int sum, n1, n2;
+            sum = add(30 , 40);
+            cout << sum;
+
+  return 0;
+}
