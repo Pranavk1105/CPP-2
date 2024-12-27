87af996bbdfd61de34f70486d36f707fd75c70d1
Pranavk1105 <pranavkale2806@gmail.com>
2024-12-27 21:43:59 +0530

    Add files via upload

---
+++ cstyle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+            char str1[10]= "hello";
+            char str2[10]="world";
+            int len ;
+
+            strcpy (str3 , str1);
+            cout << "strcpy(str3,str1) :" << str3 << endl;
+            
+  return 0;
+}
