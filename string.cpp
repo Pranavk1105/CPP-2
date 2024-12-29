a236952e2bbada585eded26f5d6fbcd873539ac8
Pranavk1105 <pranavkale2806@gmail.com>
2024-12-29 11:29:58 +0530

    Add files via upload

---
+++ string.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main() 
+{
+            char str1[10]= "hello";
+            char str2[10]="world";
+            cout << str1 << '\n';
+            cout << str2 ;
+            return 0;
+  return 0;
+}
