2c6472af7d5c2a41578f482368332940c9a401a4
Pranavk1105 <pranavkale2806@gmail.com>
2024-12-29 11:30:07 +0530

    Add files via upload

---
+++ string2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+            char str[100];
+            cout << "Enter a string: ";
+            cin >> str;
+            cout << "\n Enter another string: " << str << endl;
+            cin >> str;
+            cout << "you enterd: " << str << endl;
+  return 0;
+}
