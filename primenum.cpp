9d18faa67002a630fdf58f7f1798c3ddce258d61
Pranavk1105 <pranavkale2806@gmail.com>
2024-12-29 11:28:00 +0530

    Add files via upload

---
+++ primenum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int checkprime(int num){
+            int num;
+            cout << "Please enter the number :";
+            cin >> num;
+            
+
+}
+int main() 
+{
+  return 0;
+}
