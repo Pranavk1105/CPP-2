550ac1acaae9d548565b9a9527f6377a8ba1e13b
Pranavk1105 <pranavkale2806@gmail.com>
2024-12-29 11:27:34 +0530

    Add files via upload

---
+++ numberchecker.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+            int num;
+            cout<< "enter the number:";
+            cin >> num;
+            if (num>=0){
+                        if (num%2==0)
+                        cout<< "this is a positive even number";
+                        else
+                        cout << "this is a positive odd number";
+            }
+            else if (num==0){
+                        cout << "number is zero";
+            }
+            else;
+            cout << "number is negetive";
+            return 0;
+            
+            
+}
