81b93bbc2a85fc6cf7e6be8eeda935e59daa5051
Pranavk1105 <pranavkale2806@gmail.com>
2024-12-29 11:29:48 +0530

    Add files via upload

---
+++ squarearea.cpp
@@ -0,0 +1,12 @@
+#include <iostream> //header file
+using namespace std; //standard namespace
+int main(){
+            int l, area;  //variable declaration with there datatype
+            cout<<"please enter the side length:";
+            cin >> l;
+            cout <<" area of square is";
+            area = l*l;
+            cout <<" = "<< area << endl;
+            return 0;
+
+}
